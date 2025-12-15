af6353901f213083c5837d5cfbce7a1f02f643e0
Your Name <you@example.com>
2025-12-15 08:54:45 +0100

    add pompe-hs/process.py

--- of_protocols/Pompe-HS/scripts/process.py
+++ of_protocols/Pompe-HS/scripts/process.py
@@ -16,9 +16,13 @@ for file in files:
     if sys.argv[1] in file:
         f = open(sys.argv[2] + "/" + file, "r")
         for line in f.readlines():
-            count = count + 1
-            latencies.append(float(line.split()[-1]))
-            average = average + float(line.split()[-1])
+            try:
+                latency = float(line.split()[-1])
+                count = count + 1
+                latencies.append(latency)
+                average = average + float(latency)
+            except Exception as e:
+                pass
 
 latencies.sort()
 
@@ -28,7 +32,7 @@ latency99 = latencies[int(count * 0.99)]
 average = average / count
 
 # print(latencies)
-print("Total:", count, " transactions")
+print("Total tx count: ", count)
 print("50%: ", latency50 * 1000)
 print("90%: ", latency90 * 1000)
 print("99%: ", latency99 * 1000)
