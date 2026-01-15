6296eb669f6104a3e54ecdf680a7f5c27200d783
Your Name <you@example.com>
2026-01-15 11:39:56 +0100

    node logs tx_digest receival

--- worker/src/local_order_maker.rs
+++ worker/src/local_order_maker.rs
@@ -170,8 +170,16 @@ impl LocalOrderMaker {
 
         loop {
             tokio::select! {
+
                 Some(tx_digest) = self.rx_tx_digests.recv() => {
                     if !self.tx_digest_to_order.contains_key(&tx_digest) {
+
+                        log::info!(
+                            "Receival of tx_digest {} at position {}", 
+                            tx_digest, 
+                            self.global_tx_counter
+                        );
+
                         self.tx_digest_to_order.insert(tx_digest.clone(), self.global_tx_counter);
                         self.global_tx_counter += 1;
                         
