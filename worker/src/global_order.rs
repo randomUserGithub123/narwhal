d5454fd862187baf8643bb6f4176c4a3eb8c0de3
Your Name <you@example.com>
2026-01-22 10:42:25 +0100

    matrix_pool_size is 20

--- worker/src/global_order.rs
+++ worker/src/global_order.rs
@@ -29,7 +29,7 @@ use crate::batch_maker::Batch;
 
 const MAX_TX: usize = 65_535;
 const MAX_ORDERS: usize = 500;  // Max LocalOrders per subdag
-const MATRIX_POOL_SIZE: usize = 10; // M
+const MATRIX_POOL_SIZE: usize = 20; // M
 
 pub struct UTIGMatrix {
     // === UTIG fields ===
