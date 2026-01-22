09fe925982903213fda83ce381eff5e2b9c2131b
Your Name <you@example.com>
2026-01-22 09:14:49 +0100

    channel capacity at worker set to 100k

--- worker/src/worker.rs
+++ worker/src/worker.rs
@@ -28,7 +28,7 @@ use tokio::sync::mpsc::{channel, Sender};
 pub mod worker_tests;
 
 /// The default channel capacity for each channel of the worker.
-pub const CHANNEL_CAPACITY: usize = 1_000;
+pub const CHANNEL_CAPACITY: usize = 100_000;
 
 /// The primary round number.
 // TODO: Move to the primary.
