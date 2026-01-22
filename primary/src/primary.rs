316ace48207e1ed81ced7299002a15621c0929fe
Your Name <you@example.com>
2026-01-22 10:24:43 +0100

    channel_capacity 100k at primary

--- primary/src/primary.rs
+++ primary/src/primary.rs
@@ -25,7 +25,7 @@ use store::Store;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 /// The default channel capacity for each channel of the primary.
-pub const CHANNEL_CAPACITY: usize = 1_000;
+pub const CHANNEL_CAPACITY: usize = 100_000;
 
 /// The round number.
 pub type Round = u64;
